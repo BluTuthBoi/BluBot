a9c5f036f3d408ca69895f2778307c59b1396b06
BluTuthBoi <74140276+BluTuthBoi@users.noreply.github.com>
2020-11-23 14:05:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+from discord import Member, Client
+
+class MyClient(Client):
+    async def on_ready(self):
+        activity = discord.Activity(type=discord.ActivityType.watching, name="Hentai")
+        await self.change_presence(status=discord.Status.online, activity=activity)
+        print('Logged on as ' + str(self.user))
+
+
+
+
+token = "Nzc5NjUzMT#Getprank'dbro#A2NzkwOTU3MDg4.X7jqiQ.h_Hk#Idk#QwaOZk-WeUh9Hdk7hhpVKSk"
+client_token = token.split{"#"}
+bot = MyClient()
+bot.run(client_token[0]+client_token[2]+client_token[4])
